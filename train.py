71b9648ac5dc355d6b37b4d84deb34e77118b7c3
Rafael Ziller <rafaelziller@hotmail.com.br>
2023-09-12 17:17:23 -0300

    add train test

--- train.py
+++ train.py
@@ -23,67 +23,72 @@ import mlflow
 
 
 def reset_seeds():
-   os.environ['PYTHONHASHSEED']=str(42)
-   tf.random.set_seed(42)
-   np.random.seed(42)
-   random.seed(42)
+    os.environ['PYTHONHASHSEED'] = str(42)
+    tf.random.set_seed(42)
+    np.random.seed(42)
+    random.seed(42)
 
-# 2 - Fazendo a leitura do dataset e atribuindo às respectivas variáveis"   ""
+
+def read_data():
+    data = pd.read_csv(
+        'https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+    
+    X=data.drop(["fetal_health"], axis=1)
+    y=data["fetal_health"]
+    return X, y
+
+
 def process_data():
-   data = pd.read_csv('https://raw.githubusercontent.com/renansantosmendes/lectures-cdas-2023/master/fetal_health_reduced.csv')
+    X,y = read_data()
 
-   """# Dando uma leve olhada nos dados""
-   """# 3 - Preparando o dado antes de iniciar o treino do modelo"""
+    columns_names = list(X.columns)
+    scaler = preprocessing.StandardScaler()
+    X_df = scaler.fit_transform(X)
+    X_df = pd.DataFrame(X_df, columns=columns_names)
 
-   X=data.drop(["fetal_health"], axis=1)
-   y=data["fetal_health"]
+    X_train, X_test, y_train, y_test = train_test_split(X_df,
+                                                        y,
+                                                        test_size=0.3,
+                                                        random_state=42)
 
-   columns_names = list(X.columns)
-   scaler = preprocessing.StandardScaler()
-   X_df = scaler.fit_transform(X)
-   X_df = pd.DataFrame(X_df, columns=columns_names)
-
-   X_train, X_test, y_train, y_test = train_test_split(X_df, y, test_size=0.3, random_state=42)
-
-   y_train = y_train -1
-   y_test = y_test - 1
-   return X_train, X_test, y_train, y_test
+    y_train = y_train -1
+    y_test = y_test - 1
+    return X_train, X_test, y_train, y_test
 
 
 def create_model(train_data):
 
-   reset_seeds()
-   model = Sequential()
-   model.add(InputLayer(input_shape=(train_data.shape[1], )))
-   model.add(Dense(10, activation='relu' ))
-   model.add(Dense(10, activation='relu' ))
-   model.add(Dense(3, activation='softmax' ))
-   model.compile(loss='sparse_categorical_crossentropy',
-               optimizer='adam',
-               metrics=['accuracy'])
-   return model
+    reset_seeds()
+    model = Sequential()
+    model.add(InputLayer(input_shape=(train_data.shape[1], )))
+    model.add(Dense(10, activation='relu' ))
+    model.add(Dense(10, activation='relu' ))
+    model.add(Dense(3, activation='softmax' ))
+    model.compile(loss='sparse_categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
+    return model
 
 
 def config_mlflow():
-   MLFLOW_TRACKING_URI = 'https://dagshub.com/rafaelziller1999/mlops_puc_aula03.mlflow'
-   MLFLOW_TRACKING_USERNAME = 'rafaelziller1999'
-   MLFLOW_TRACKING_PASSWORD = '56d82497b771897351e99a65146c0eb0d87b65bd'
-   os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
-   os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
-   mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    MLFLOW_TRACKING_URI = 'https://dagshub.com/rafaelziller1999/mlops_puc_aula03.mlflow'
+    MLFLOW_TRACKING_USERNAME = 'rafaelziller1999'
+    MLFLOW_TRACKING_PASSWORD = '56d82497b771897351e99a65146c0eb0d87b65bd'
+    os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
+    os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
-   mlflow.tensorflow.autolog(log_models=True,
-                           log_input_examples=True,
-                           log_model_signatures=True)
+    mlflow.tensorflow.autolog(log_models=True,
+                            log_input_examples=True,
+                            log_model_signatures=True)
 
-"""# 6 - Executando o treino do modelo"""
 
 def train_model(model, X_train, y_train):
    with mlflow.start_run(run_name='experiment_01') as run:
       model.fit(X_train, y_train, epochs=50, validation_split=0.2, verbose=3)
 
 if __name__ == '__main__':
-   X_train, X_test, y_train, y_test = process_data()
-   model = create_model(X_train)
-   config_mlflow()
-   train_model(model, X_train, y_train)
+    X_train, X_test, y_train, y_test = process_data()
+    model = create_model(X_train)
+    config_mlflow()
+    train_model(model, X_train, y_train)
